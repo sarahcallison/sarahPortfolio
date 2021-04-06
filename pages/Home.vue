38bc91b146efa40c624bbb92b3316434b77221da
tyelf22 <tyelf22@hotmail.com>
2021-04-05 21:15:04 -0600

    initial commit with background image

---
+++ pages/Home.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<h1 class="customClass">Sarah Callison</h1>
+		<p>This is her portfolio</p>
+        <button type="button" class="btn btn-warning">Primary</button>
+	</div>
+</template>
+
+<script>
+export default {
+    //use for SEO
+	head() {
+		return {
+			title: "Home",
+			meta: [
+				{
+					hid: "description",
+					name: "description",
+					content: "Sarah Callison website"
+				}
+			]
+		};
+	}
+};
+</script>
+
+<style  lang="scss" scoped>
+    .customClass {
+        color: $green;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="outerWrapper">
+  <div class="wrapper">
+    <h1>test</h1>
+  </div>
+  <div class="main">
+    <h1>test</h1>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.wrapper{
+    background-image: url(@/assets/images/heroHeaderMobile.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    height: 100vh;
+
+}
+</style>
