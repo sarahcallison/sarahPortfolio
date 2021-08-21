39eb458d2cc9f6e4aee1c929605be2f6dc69d310
sarahcallison <43583539+sarahcallison@users.noreply.github.com>
2021-08-21 14:18:03 -0600

    add linear gradient to background

--- pages/About.vue
+++ pages/About.vue
@@ -2,7 +2,7 @@
 	<div>
 		<Nav where="top" shadow="0px 8px 10px -10px #999" />
 		<div
-			style="background: #eeeeee; position: relative; min-height: 100vh;"
+			style="background: linear-gradient(to left, #f5f5f5, #d7d7d7); position: relative; min-height: 100vh;"
 		>
 			<div class="container-fluid aboutContainer">
 				<div class="row">
