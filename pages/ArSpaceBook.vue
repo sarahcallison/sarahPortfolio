2b5acedca46dbd56b19ced6ef44326f04008c6d9
sarahcallison <43583539+sarahcallison@users.noreply.github.com>
2021-10-12 18:33:42 -0600

    text size and width

--- pages/ArSpaceBook.vue
+++ pages/ArSpaceBook.vue
@@ -473,11 +473,12 @@ export default {
 			}
 
 			p {
-				font-size: 18px;
+				font-size: 16px;
 			}
 
 			figcaption {
 				color: $bodyGray;
+				padding-bottom: 40px;
 			}
 
 			.purpHeader {
@@ -572,7 +573,7 @@ export default {
 			}
 			
 			.mainText p {
-				width: 700px;
+				width: 600px;
 				margin: 0 auto;
 			}
 			
