a896caa44396c48e7924808cd626f73950dfb4da
sarahcallison <43583539+sarahcallison@users.noreply.github.com>
2021-10-28 14:48:07 -0600

    article text

--- pages/userResearch.vue
+++ pages/userResearch.vue
@@ -46,17 +46,27 @@
 						>
 							<h2 id="problemHeader">The Problem</h2>
 							<p>
-								Lorem ipsum dolor sit amet, consectetur
-								adipiscing elit. Nullam dapibus convallis sapien
-								sed egestas. Nunc suscipit sem lorem, non
-								pretium risus rutrum ut. Mauris sagittis urna
+								The onset of the COVID-19 pandemic forced medical providers 
+								to rapidly adopt Telehealth in 2020. An expected three-year 
+								timeframe for remote Telehealth implementation was completed 
+								in an unprecedented three weeks. Improving the user experience 
+								of remote digital healthcare became a global priority.  
+							<br />
+							<br />
+							My senior project group was tasked with improving the Telehealth experience for users in rural Tahiti. We were unable to travel to Tahiti due to COVID international travel restrictions, so we focused on improving the Telehealth experience locally. We were responsible for the research phase of the project. 
+							<br />
+							<br />
+							I was responsible for creating the interactive infographic, writing the survey and interview questions, and writing the academic research report.
 							</p>
+							
 
 							<h4 class="purpHeader">Deliverables</h4>
-							<p class="highFidText">High-fidelity prototype</p>
+							<p class="highFidText">Interactive infographic</p>
+							<p class="highFidText">Academic research report</p>
+							<p class="highFidText">Provider personas</p>	
 							<GradButton
-								title="Go To XD Adobe.com"
-								url="https://xd.adobe.com/view/729e9ab2-12f3-4c6f-9d05-5ce0775a0fbd-27cd/"
+								title="View infographic"
+								url="https://sarahcallison.github.io/"
 							/>
 						</div>
 					</div>
@@ -75,114 +85,134 @@
 							</h4>
 
 							<p class="text-left">
-								Lorem ipsum dolor sit amet, consectetur
-								adipiscing elit. Nullam dapibus convallis sapien
-								sed egestas. Nunc suscipit sem lorem, non
-								pretium risus rutrum ut. Mauris sagittis urna
-								elit, efficitur convallis nisi feugiat quis. Ut
-								lacus est, egestas vel feugiat in, vehicula quis
-								tortor. Cras pretium odio nisl, eget dignissim
-								nunc commodo et.
+								*Image of telehealth cover*
 								<br />
 								<br />
-
-								Aliquam hendrerit vitae tortor id iaculis.
-								Vestibulum varius quam eros, sit amet mattis
-								nulla varius vel. Maecenas feugiat mi quis
-								mattis ultricies. Morbi vitae lectus in erat
-								blandit egestas. Vestibulum ante ipsum primis in
-								faucibus orci luctus et ultrices posuere cubilia
-								curae; Mauris ultricies blandit metus, et cursus
-								urna congue ut. Mauris mollis tortor eget nisl
-								scelerisque sagittis. Etiam nisl orci, malesuada
-								et aliquet vel, congue sit amet ex. Curabitur
-								tincidunt lacinia ligula, eu pellentesque lectus
-								vestibulum vitae. Sed eu mauris eu sem viverra
-								aliquet. Aliquam erat volutpat. Sed lacinia
-								massa ornare rhoncus facilisis.
+								We began the research phase of this project by completing a review of current Telehealth research. As existing literature focuses primarily on provider acceptance of the use of Telehealth in medical practice, exploratory research to pinpoint user issues experienced by both the provider and the patient was needed. Identifying the underlying cause of these issues would serve as a guide for further research and, in turn, illuminate potential solutions for improving the Telehealth user experience. 
+								<br />
+								<br />
+								After speaking with faculty advisors from the Digital Media and Public and Community Health departments, we came up with these questions to guide and focus research tasks: 
+								<ol>
+								<li>What does a provider need to comfortably take care of patients remotely?</li>
+								<li>How are current digital solutions meeting or not meeting provider needs?</li>
+								<li>What gaps in patient care need to be filled?</li>	
+								</ol>
+								A focus on providers in the first phase of our project would serve to guide future research into how issues in the provider user experience impact patient outcomes. 
+								<br />
+								<br />
+								*Image of app research icons*
+								<br />
+								<br />
+								One of the issues that was discussed in the early stages of this research is poor Internet connectivity. Developing a medical resource app that would be available offline was considered. In an audit of current medical resource technology, 16 apps with user reviews of four stars or higher were identified, seven of which are available offline. We refocused our research activities on how to improve existing digital solutions. 
+								<br />
+								<br />
+								In a review of the literature regarding design research methodologies, two concepts were emphasized repeatedly by UX researchers: the importance of variety in obtaining valid results and the use of research as a tool to fuel the design process. 
+								Instead of designing a perfect quantitative study with a large N, researching a diverse set of circumstances can save time, be cost-effective, and deliver useful insights. Sample size is important; however, a large sample of very similar participants may produce results that are not representative of the target audience as a whole. For results that are externally valid, a diverse set of users and conditions should be tested.
+								<br />
+								<br />
+								Since time is frequently a limitation, designing a perfect study may not be feasible. Researchers from the Nielsen Norman Group, a premier resource in UX research and design, say that it is most important to start somewhere and keep learning. Research activities should be planned and executed to inform the next step in the design process.
 							</p>
 
-							<h4 class="text-center subtitle">Ux Framework</h4>
+							<h4 class="text-center subtitle">Methods</h4>
 							
 
 							<p>
-								Lorem ipsum dolor sit amet, consectetur
-								adipiscing elit. Nullam dapibus convallis sapien
-								sed egestas. Nunc suscipit sem lorem, non
-								pretium risus rutrum ut. Mauris sagittis urna
-								elit.
+								For a more accurate analysis of the Telehealth provider experience, quantitative and qualitative user research methods were utilized. To first identify, and then quantify provider user experience issues, we created a 10-question, multiple choice survey using the Qualtrics platform. This survey was designed to be taken by providers in approximately two minutes. Question topics included how digital solutions are used by providers in daily practice and what issues are encountered during digital solutions usage. 
 								<br />
 								<br />
 
-								Aliquam hendrerit vitae tortor id iaculis.
-								Vestibulum varius quam eros, sit amet mattis
-								nulla varius vel. Maecenas feugiat mi quis
-								mattis ultricies. Morbi vitae lectus in erat
-								blandit egestas. Vestibulum ante ipsum primis in
-								faucibus orci luctus et ultrices posuere cubilia
-								curae;
+								The survey link was sent to twenty healthcare clinics and a teletherapy organization, all located within the United States. Additionally, the survey was advertised on the personal social media channels of the researchers. No incentive was offered for completion of the survey. 
+								<br />
+								<br />
+								User interviews were utilized to gather qualitative data. The final question of the survey asked providers to share their email address if they would be willing to answer additional questions regarding their Telehealth user experience. The SignUp Genius platform was used to schedule interview time slots. Providers were invited in an email to sign up for a 30-minute remote interview which would take place on Zoom. Interview invitations included a digital recording release form. 
+								<br />
+								<br />
+								Interview questions were designed to probe for the underlying cause of Telehealth user experience issues without leading the interviewee to give expected answers. Providers were asked to identify which user experience issue had the greatest impact on their ability to provide medical care. Additional questions asked providers to describe the functionality of digital solutions given an ideal world. A script was used to standardize each interview and to avoid variations in interview questions that may have biased the responses.
 							</p>
 
-							<h4 class="text-center subtitle">Ideation</h4>
+							<h4 class="text-center subtitle">Results</h4>
 							
 
 							<p>
-								Lorem ipsum dolor sit amet, consectetur
-								adipiscing elit. Nullam dapibus convallis sapien
-								sed egestas. Nunc suscipit sem lorem, non
-								pretium risus rutrum ut. Mauris sagittis urna
-								elit.
+								49 clinicians in 17 different medical professions completed the survey. Of the 49 respondents, 58.5 percent utilize a digital solution to find information regarding best practice at least once per day. 26.8 percent of the survey sample uses digital solutions for this purpose multiple times per day. 
 								<br />
 								<br />
-
-								Aliquam hendrerit vitae tortor id iaculis.
-								Vestibulum varius quam eros, sit amet mattis
-								nulla varius vel. Maecenas feugiat mi quis
-								mattis ultricies. Morbi vitae lectus in erat
-								blandit egestas. Vestibulum ante ipsum primis in
-								faucibus orci luctus et ultrices posuere cubilia
-								curae;
+								*Graphs*
+								<br />
+								<br />
+								The frequency in use of digital solutions as a resource for medical information was not surprising. However, lack of Internet connectivity or a digital solution “not working” occurred within our sample population less frequently than was expected. Issues were experienced only several times per month by 43.9 percent of our sample. Another 43.9 percent never experienced these issues. 
+								<br />
+								<br />
+								*Graphs*
+								<br />
+								<br />
+								Although these issues were experienced relatively infrequently, 61 percent of our respondents indicated that their ability to provide care was impacted when their Internet or digital solution was not working. 
+								<br />
+								<br />
+								*Graphs*
+								<br />
+								<br />
+								Additional issues were experienced as providers navigated the UI of their digital solution. 34 percent of our sample stated that their digital solution was difficult to use or navigate. 
+								<br />
+								<br />
+								*Graphs*
+								<br />
+								<br />
+								View an *interactive infographic* of the survey results. 
+								<br />
+								<br />
+								One of the proponents of user experience research is that three to five percent of participants in a usability study identify 85 percent of the usability issues. Of the 49 respondents, 88 percent were willing to participate in a user experience interview, however, only six providers responded to our interview invites. Interviews were conducted with each of the six providers who practice in the following care areas: nursing and nurse education, speech therapy, psychology, respiratory therapy, and certified nurse midwifery. 
+								<br />
+								<br />
+								Although technical issues such as Internet 
+								connectivity were less frequent than expected, 
+								user experience issues were noted by each 
+								provider during their interview. Three of the 
+								user experience problems mentioned in the 
+								interviews were identified by providers as having
+								a significant impact on their ability to provide 
+								care: difficulty with patient onboarding to 
+								Telehealth technology, searchability or navigation 
+								of digital solutions, and issues with charting, 
+								such as a non-intuitive user interface in the 
+								charting platform. 
 							</p>
 
-							<h4 class="text-center subtitle">Ui Design</h4>
+							<h4 class="text-center subtitle">Next Steps</h4>
 						
 
 							<p>
-								Lorem ipsum dolor sit amet, consectetur
-								adipiscing elit. Nullam dapibus convallis sapien
-								sed egestas. Nunc suscipit sem lorem, non
-								pretium risus rutrum ut. Mauris sagittis urna
-								elit.
+								This research did not focus on the patient 
+								user experience or how issues experienced by 
+								providers impact patient outcomes. Research that 
+								explores the patient user experience and patient 
+								outcomes is essential to prioritize the significance 
+								of user experience issues and to guide future 
+								research and design decisions. Direct observation of 
+								user behavior in a field study may be useful in 
+								avoiding response biases.
 								<br />
 								<br />
-
-								Aliquam hendrerit vitae tortor id iaculis.
-								Vestibulum varius quam eros, sit amet mattis
-								nulla varius vel. Maecenas feugiat mi quis
-								mattis ultricies. Morbi vitae lectus in erat
-								blandit egestas. Vestibulum ante ipsum primis in
-								faucibus orci luctus et ultrices posuere cubilia
-								curae;
-							</p>
-
-							<h4 class="text-center subtitle">Iteration</h4>
-							
-
-							<p>
-								Lorem ipsum dolor sit amet, consectetur
-								adipiscing elit. Nullam dapibus convallis sapien
-								sed egestas. Nunc suscipit sem lorem, non
-								pretium risus rutrum ut. Mauris sagittis urna
-								elit.
+								Four provider *personas*(link)were 
+								created as a tool for future researchers. After 
+								exploratory research based on the patient user 
+								experience is completed, patient personas should 
+								be utilized to guide the design process. Proposed 
+								solutions of a chatbot and/or voicebot, and voice-enabled 
+								search and charting will need to be re-evaluated based 
+								on the results of patient user research. 
 								<br />
 								<br />
-								Aliquam hendrerit vitae tortor id iaculis.
-								Vestibulum varius quam eros, sit amet mattis
-								nulla varius vel. Maecenas feugiat mi quis
-								mattis ultricies. Morbi vitae lectus in erat
-								blandit egestas. Vestibulum ante ipsum primis in
-								faucibus orci luctus et ultrices posuere cubilia
-								curae;
+								*Image of part of persona*
+								<br />
+								<br />
+								Once research is completed, the design process 
+								should include testing of both low- and high-fidelity 
+								prototypes before building the design. User testing 
+								of prototypes is essential to evaluate the validity 
+								of design decisions. After the design is built, the 
+								final step in the design process should include user 
+								testing to identify any user experience issues. 
 							</p>
 
 							<img
@@ -202,28 +232,6 @@
 							<figcaption class="text-center mt-3">
 								Second Iteration
 							</figcaption>
-
-							<h4 class="text-center subtitle">
-								Problem Solving
-							</h4>
-							
-
-							<p>
-								Lorem ipsum dolor sit amet, consectetur
-								adipiscing elit. Nullam dapibus convallis sapien
-								sed egestas. Nunc suscipit sem lorem, non
-								pretium risus rutrum ut. Mauris sagittis urna
-								elit.
-								<br />
-								<br />
-								Aliquam hendrerit vitae tortor id iaculis.
-								Vestibulum varius quam eros, sit amet mattis
-								nulla varius vel. Maecenas feugiat mi quis
-								mattis ultricies. Morbi vitae lectus in erat
-								blandit egestas. Vestibulum ante ipsum primis in
-								faucibus orci luctus et ultrices posuere cubilia
-								curae;
-							</p>
 
 							<div class="embed-responsive embed-responsive-4by3">
 								<iframe
@@ -251,30 +259,11 @@
 						</h4>
 
 						<p class="text-left">
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit. Nullam dapibus convallis sapien sed egestas.
-							Nunc suscipit sem lorem, non pretium risus rutrum
-							ut. Mauris sagittis urna elit, efficitur convallis
-							nisi feugiat quis. Ut lacus est, egestas vel feugiat
-							in, vehicula quis tortor. Cras pretium odio nisl,
-							eget dignissim nunc commodo et.
-							<br />
-							<br />
-
-							Aliquam hendrerit vitae tortor id iaculis.
-							Vestibulum varius quam eros, sit amet mattis nulla
-							varius vel. Maecenas feugiat mi quis mattis
-							ultricies. Morbi vitae lectus in erat blandit
-							egestas. Vestibulum ante ipsum primis in faucibus
-							orci luctus et ultrices posuere cubilia curae;
-							Mauris ultricies blandit metus, et cursus urna
-							congue ut. Mauris mollis tortor eget nisl
-							scelerisque sagittis. Etiam nisl orci, malesuada et
-							aliquet vel, congue sit amet ex. Curabitur tincidunt
-							lacinia ligula, eu pellentesque lectus vestibulum
-							vitae. Sed eu mauris eu sem viverra aliquet. Aliquam
-							erat volutpat. Sed lacinia massa ornare rhoncus
-							facilisis.
+							The user experience design process should be cyclical. 
+							As technology and user needs evolve, additional research 
+							and user testing is needed. With a multitude of tools and 
+							resources available, designers are well-equipped to keep 
+							pace with the rapidly changing Telehealth industry. 
 						</p>
 
 						<h4 class="purpHeader text-center">Deliverables</h4>
