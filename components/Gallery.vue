16a1be94a6591bb7e75e0976332358093d8e0131
sarahcallison <43583539+sarahcallison@users.noreply.github.com>
2022-08-22 17:35:06 -0600

    remove interaction design and switch out audrey video for infographic

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -75,11 +75,11 @@
 				>
 					<NuxtLink
 						class="toImageNuxtLink"
-						url="@/assets/images/gallery/timothy.jpg"
+						url="@/assets/images/gallery/x.jpg"
 						:to="{
 							name: 'ImgBackground',
 							params: {
-								img: 'timothy'
+								img: 'x'
 							}
 						}"
 					>
@@ -137,31 +137,12 @@
 			<div class="row mb-5">
 				<div class="col-12 align-self-center textSection">
 					
-					<h2>Interaction Design</h2>
+					
+			<!-- Motion Graphics -->
+			<div class="row my-5">
+				<div class="col-12 align-self-center textSection">
+					<h2>Motion Graphics</h2>
 					<div class="gradLine"></div>
-				</div>
-				<div
-					class="col-12 px-0 px-md-2 px-lg-3 col-md-4 my-4 align-self-start"
-				>
-					<NuxtLink
-						class="toImageNuxtLink"
-						:to="{
-							name: 'ImgBackground',
-							params: {
-								vid: `https://www.youtube.com/embed/5lG3VprBGeQ`
-							}
-						}"
-					>
-						<img
-							class="img-fluid"
-							src="~/assets/images/gallery/tabletMag.jpg"
-							alt="image of tablet magazine"
-						/>
-					</NuxtLink>
-
-					<p class="text-center mt-3 px-1">
-						Demo of interactive magazine concept for tablet
-					</p>
 				</div>
 				<div
 					class="col-12 px-0 px-md-2 px-lg-3 col-md-4 my-4 align-self-start"
@@ -179,59 +160,6 @@
 					</a>
 					<p class="text-center  mt-3 px-1">
 						Interactive infographic made in Hype
-					</p>
-				</div>
-				<div
-					class="col-12 px-0 px-md-2 px-lg-3 col-md-4 my-4 align-self-start"
-				>
-					<NuxtLink
-						class="toImageNuxtLink"
-						:to="{
-							name: 'ImgBackground',
-							params: {
-								vid: `https://www.youtube.com/embed/wigsdEFgrBQ`
-							}
-						}"
-					>
-						<img
-							class="img-fluid "
-							src="~/assets/images/gallery/fancySprinkles.jpg"
-							alt="image of tablet magazine"
-						/>
-					</NuxtLink>
-					<p class="text-center  mt-3 px-1">
-						Demo of interactive magazine concept for Fancy Sprinkles
-					</p>
-				</div>
-			</div>
-
-			<!-- Motion Graphics -->
-			<div class="row my-5">
-				<div class="col-12 align-self-center textSection">
-					<h2>Motion Graphics</h2>
-					<div class="gradLine"></div>
-				</div>
-				<div
-					class="col-12 px-0 px-md-2 px-lg-3 col-md-4 my-4 align-self-start"
-				>
-					<NuxtLink
-						class="toImageNuxtLink"
-						:to="{
-							name: 'ImgBackground',
-							params: {
-								vid: `https://www.youtube.com/embed/BX3ImMPQWqI`
-							}
-						}"
-					>
-						<img
-							class="img-fluid"
-							src="@/assets/images/gallery/hypeGif.jpg"
-							alt="image of Audrey Hepburn"
-						/>
-					</NuxtLink>
-
-					<p class="text-center px-1 mt-3">
-						Motion graphic created in Hype
 					</p>
 				</div>
 				<div
