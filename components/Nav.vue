296ff4e44d86d8dbac68dfd2ce03d6c124fc7a48
sarahcallison <43583539+sarahcallison@users.noreply.github.com>
2022-12-21 19:28:59 -0700

    increase size of nav li

--- components/Nav.vue
+++ components/Nav.vue
@@ -88,7 +88,7 @@ export default {
 					text-decoration: none;
 					letter-spacing: $tracking250;
 					text-transform: uppercase;
-					font-size: 13px;
+					font-size: 14px;
 
 					&:hover {
 						text-decoration: none;
